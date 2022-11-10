93f4d7d4e990338eabde407cbc5694819d42388e
idonacmias <108004738+idonacmias@users.noreply.github.com>
2022-11-10 16:23:57 +0200

    Add files via upload
    uplode the project again, with modification

--- new_app/app.py
+++ new_app/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask import render_template
+from models import get_db
+
+from datetime import date
+
 api = Flask(__name__)
  
 @api.route('/')
 def index():
-    return 'Hello, World!'
+    context = {'first_name' : 'bob', 'last_name' : 'marly', 'date_of_birth': date(2020,11,30)}
+    # cherecter = Cherecter('ido') 
+    # context = {'name': cherecter.name}
+    return render_template('init.html', context=context)    
  
 @api.route('/new_route/')
 def new_route():
